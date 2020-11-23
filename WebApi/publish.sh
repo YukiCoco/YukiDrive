849136b209493df651270e61719a6408b42f2f65
YukiCoco <theblackrains@outlook.com>
2020-11-23 20:40:04 +0800

    修改编译脚本

--- WebApi/publish.sh
+++ WebApi/publish.sh
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-version="1.1.1"
-
-dotnet publish --runtime linux-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/YukiDrive-${version}-linux-x64 -c Release
-dotnet publish --runtime linux-arm --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/YukiDrive-${version}-linux-arm -c Release
-dotnet publish --runtime osx-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/YukiDrive-${version}-macOS -c Release
-dotnet publish --runtime win-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/YukiDrive-${version}-win -c Release
-=======
 version="1.1.2"
 
 dotnet publish --runtime linux-x64 -p:PublishSingleFile=true --self-contained true -o ./Release/WebApi/${version}/YukiDrive-${version}-linux-x64 -c Release
 dotnet publish --runtime linux-arm -p:PublishSingleFile=true --self-contained true -o ./Release/WebApi/${version}/YukiDrive-${version}-linux-arm -c Release
 dotnet publish --runtime osx-x64 -p:PublishSingleFile=true --self-contained true -o ./Release/WebApi/${version}/YukiDrive-${version}-macOS -c Release
 dotnet publish --runtime win-x64 -p:PublishSingleFile=true --self-contained true -o ./Release/WebApi/${version}/YukiDrive-${version}-win -c Release
->>>>>>> dev
