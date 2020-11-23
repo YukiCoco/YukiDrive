041aac38f0ff704fa66e1983e9e31c7e7a804691
YukiCoco <theblackrains@outlook.com>
2020-11-23 17:38:42 +0800

    修改 gitignore

--- WebApi/publish.sh
+++ WebApi/publish.sh
@@ -1,6 +1,6 @@
-version="1.1.0"
+version="1.1.1"
 
-dotnet publish --runtime linux-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/linux-x64 -c Release
-dotnet publish --runtime linux-arm --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/linux-arm -c Release
-dotnet publish --runtime osx-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/macOS -c Release
-dotnet publish --runtime win-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/win -c Release
+dotnet publish --runtime linux-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/YukiDrive-${version}-linux-x64 -c Release
+dotnet publish --runtime linux-arm --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/YukiDrive-${version}-linux-arm -c Release
+dotnet publish --runtime osx-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/YukiDrive-${version}-macOS -c Release
+dotnet publish --runtime win-x64 --self-contained true -o /Users/yukino/Desktop/Projects/YukiDrive/Release/WebApi/${version}/YukiDrive-${version}-win -c Release
